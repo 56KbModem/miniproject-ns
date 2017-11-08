37a179761c2cbad2ee1266de9d28ccb9f67312e5
Nathan Hoekstra <nathan.hoekstra@student.hu.nl>
2017-11-08 15:45:59 +0100

    Added analytics from branch to main gui, removed autocomplete

--- gui.py
+++ gui.py
@@ -1,15 +1,7 @@
 from tkinter import *
 from tkinter import font
-from autocomplete import AutocompleteEntry
 import api_interface
-import re
-
-# Setup autocomplete list for testing purposes
-autocompletelist = api_interface.get_station_list()
-
-def matches(fieldValue, acListEntry):
-	pattern = re.compile(re.escape(fieldValue) + '.*', re.IGNORECASE)
-	return re.match(pattern, acListEntry)
+import json
 
 # ---- Setup main window -----
 root = Tk()
@@ -57,8 +49,7 @@ def station_info():
 	info_label.place(x=5, y=7)
 
 	# Enter Station entry
-	#station_entry = Entry(input_box)
-	station_entry = AutocompleteEntry(autocompletelist, input_box, listboxLength=6, width=23, highlightbackground='blue', matchesFunction=matches)
+	station_entry = Entry(input_box, highlightbackground='blue')
 	station_entry.place(x=150, y=5)
 
 	# Entry Station submit
@@ -177,7 +168,59 @@ def reisplanner():
 	reisplanner_listbox = Listbox(output_frame, yscrollcommand = reisplanner_scrollbar.set, width=73)
 	reisplanner_listbox.place(x=10, y=25)
 
+# ----- ANALYTICS WINDOW -----
+def analytics():
+	def reload_json():
+		global json_data
+		json_file = open("config.json", 'r')
+		json_data = json.load(json_file)
+		json_file.close()
 
+		last_query_time_text['text'] = json_data['last_query_time']
+		last_query_text['text'] = json_data['last_query']
+		last_query_requests['text'] = json_data['requests']
+
+	analytics = Toplevel()
+	analytics.title('Statistieken')
+	analytics.geometry('1100x400')
+	analytics.resizable(width=False, height=False)
+	analytics.configure(bg='white')
+	default_font = font.Font(family='Monaco', size=12)
+	analytics.option_add('*Font', default_font)
+
+	statistieken_label = Label(analytics, text='Applicatie statistieken')
+	statistieken_label.place(x=10, y=10)
+
+	# Main window
+	stats_frame = Frame(analytics, height=300, width=1000, bd=2, relief=SUNKEN)
+	stats_frame.place(x=10, y=50)
+
+
+	# tijd van laatste aanroep naar API
+	last_query_time_label = Label(stats_frame, text='Gemaakt op: ')
+	last_query_time_label.place(x=10, y=50)
+
+	last_query_time_text = Label(stats_frame, text='')
+	last_query_time_text.place(x=155, y=50)
+
+	# laatste aanroep naar API
+	last_query_label = Label(stats_frame, text='Laatste query: ')
+	last_query_label.place(x=10, y=20)
+
+	last_query_text = Label(stats_frame, text='')
+	last_query_text.place(x=155, y=20)
+
+	#aantal queries
+	last_query_requests = Label (stats_frame, text="Aanvragen:")
+	last_query_requests.place(x=10, y=80)
+
+	last_query_requests = Label(stats_frame, text='')
+	last_query_requests.place(x=155, y=80)
+
+	reload_button = Button(stats_frame, text='Herlaad', command=reload_json)
+	reload_button.place(x=400, y=100)
+
+	reload_json()
 
 
 
@@ -220,7 +263,7 @@ label = Label(master=root, text='Statistieken',
                  foreground = 'blue')
 label.pack()
 
-button3 = Button(master=root, text='Zoek statistieken')
+button3 = Button(master=root, text='Zoek statistieken', command=analytics)
 button3.pack(pady=15)
 
 label.pack()
